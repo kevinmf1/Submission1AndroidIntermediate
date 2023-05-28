4a5a69cc9f6dd49ae1ed3025035ae4b7405a1d8e
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-05-28 08:15:58 +0700

    Initial commit and last commit (maybe)

---
+++ app/src/main/java/com/example/submissionandroidintermediate/api/APIService.kt
@@ -0,0 +1,29 @@
+package com.example.submissionandroidintermediate.api
+
+import com.example.submissionandroidintermediate.*
+import com.example.submissionandroidintermediate.dataclass.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.*
+
+interface APIService {
+    @POST("register")
+    fun registUser(@Body requestRegister: RegisterDataAccount): Call<ResponseDetail>
+
+    @POST("login")
+    fun loginUser(@Body requestLogin: LoginDataAccount): Call<ResponseLogin>
+
+    @GET("stories")
+    fun getStory(
+        @Header("Authorization") token: String,
+    ): Call<ResponseStory>
+
+    @Multipart
+    @POST("stories")
+    fun uploadPicture(
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Header("Authorization") token: String
+    ): Call<ResponseDetail>
+}
---
+++ app/src/main/java/com/example/submissionandroidintermediate/customview/CVIsPasswordSame.kt
@@ -0,0 +1,81 @@
+package com.example.submissionandroidintermediate.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.example.submissionandroidintermediate.R
+
+class CVIsPasswordSame : AppCompatEditText, View.OnFocusChangeListener {
+
+    var isPasswordValid = false
+
+    init {
+        init()
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        // Set background with border
+        background = ContextCompat.getDrawable(context, R.drawable.border)
+        transformationMethod = PasswordTransformationMethod.getInstance()
+
+        // Set onFocusChangeListener to validate password
+        onFocusChangeListener = this
+
+        // Set text change listener
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validatePassword()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Do nothing
+            }
+        })
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        if (!hasFocus) {
+            validatePassword()
+        }
+    }
+
+    private fun validatePassword() {
+        val password = text.toString().trim()
+        val confirmPassword =
+            (parent as ViewGroup).findViewById<CVPassword>(R.id.RegistPassword).text.toString()
+                .trim()
+
+        isPasswordValid = password.length >= 8 && password == confirmPassword
+        error = if (!isPasswordValid) {
+            resources.getString(R.string.passwordNotMatch)
+        } else {
+            null
+        }
+    }
+}
